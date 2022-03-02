3bbcaf00cc4306b7fe3b10343aba9005c567b823
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-03-02 10:11:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleAp
+{
+    class ThreadingCreationProgram
+    {
+        
+        public static void CallToChildThread()
+        {
+            //Console.WriteLine("Child thread starts");
+        }
+        
+        static void Main()
+        {
+            ThreadStart childref = new ThreadStart(CallToChildThread);
+            Console.WriteLine("In Main: Creating the Child thread");
+            
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+            Console.ReadKey();
+        }
+    }
+}
